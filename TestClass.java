3c0977289bc43baca65047292804eb7fa16e5fb0
PH-IFSUL <pedrosantos.pf023@academico.ifsul.edu.br>
2025-05-14 10:50:59 -0300

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -15,15 +15,15 @@ public class TestClass {
     public static void main(String[] args) {
                 Plano planoBasico = new Plano("Básico", "Uso dos Equipamentos", 100.0);
 
-        //Aluno sem Desconto
-        Aluno a2 = new Aluno("Carlos Silva", "2023001", LocalDate.now());
-        a2.setPlano(planoBasico);
-        a2.verificaDesconto();
-
         //Aluno com Desconto
         Aluno a1 = new Aluno("Maria Oliveira", "2022002", LocalDate.now().minusMonths(4));
         a1.setPlano(planoBasico);
         a1.verificaDesconto();
+
+        //Aluno sem Desconto
+        Aluno a2 = new Aluno("Carlos Silva", "2023001", LocalDate.now());
+        a2.setPlano(planoBasico);
+        a2.verificaDesconto();
 
         System.out.println("\nAluno I");
         System.out.println(a1.exibirDados());
